7868a60b9f63c55bbd7f970cd678efeb801bf55f
vanfigo <rodrigo.loyola@live.com>
2019-08-13 13:43:06 -0500

    added pagination to dashboard tournaments

--- src/main/java/com/bucketdev/betapp/repository/TournamentParticipantsRepository.java
+++ src/main/java/com/bucketdev/betapp/repository/TournamentParticipantsRepository.java
@@ -1,6 +1,8 @@
 package com.bucketdev.betapp.repository;
 
 import com.bucketdev.betapp.domain.TournamentParticipants;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,6 +17,6 @@ import java.util.Set;
 public interface TournamentParticipantsRepository extends JpaRepository<TournamentParticipants, Long> {
 
     @Query("SELECT tp FROM TournamentParticipants tp JOIN tp.participants p WHERE :uid IN p.uid")
-    Set<TournamentParticipants> findByParticipantUid(@Param("uid") String uid);
+    Page<TournamentParticipants> findByParticipantUid(Pageable page, @Param("uid") String uid);
 
 }
--- src/main/java/com/bucketdev/betapp/service/impl/TournamentParticipantsServiceImpl.java
+++ src/main/java/com/bucketdev/betapp/service/impl/TournamentParticipantsServiceImpl.java
@@ -5,6 +5,8 @@ import com.bucketdev.betapp.dto.TournamentParticipantsDTO;
 import com.bucketdev.betapp.repository.TournamentParticipantsRepository;
 import com.bucketdev.betapp.service.TournamentParticipantsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
@@ -20,7 +22,7 @@ public class TournamentParticipantsServiceImpl implements TournamentParticipants
     private TournamentParticipantsRepository repository;
 
     @Override
-    public Set<TournamentParticipantsDTO> findByParticipantUid(String uid) {
-        return repository.findByParticipantUid(uid).stream().map(TournamentParticipants::toDTO).collect(Collectors.toSet());
+    public Page<TournamentParticipantsDTO> findByParticipantUid(Pageable page, String uid) {
+        return repository.findByParticipantUid(page, uid).map(TournamentParticipants::toDTO);
     }
 }
